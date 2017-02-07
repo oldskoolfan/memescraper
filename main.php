b820f6eebabf27c121076eb3f850dde38886a06a
Andrew Harris <harris.1305@gmail.com>
2017-02-06 21:28:42 -0500

    scrape thread images from /v/, get number of comments

--- main.php
+++ main.php
@@ -3,9 +3,24 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use MemeScraper\MemeScraper;
+use Symfony\Component\DomCrawler\Crawler;
 
 $scraper = new MemeScraper();
 
 $crawler = $scraper->get4ChanVideoGameBoard();
 
-var_dump($crawler);
+$imgs = $crawler->filter('div.thread')->each(function(Crawler $node, int $i) {
+    $href = $node->filter('div.fileText a')->attr('href');
+    $summary = $node->filter('span.summary');
+    if ($summary->count() === 0) {
+        $comments = $node->filter('div.replyContainer')->count();
+    } else {
+        $comments = (int)substr($summary->text(), 0, strpos($summary->text(), ' '));
+    }
+    return [
+        'href' => $href,
+        'comments' => $comments,
+    ];
+});
+
+var_dump($imgs);
